5c8a71f23cb48f9ba60b3d8d91fc9d802c26744b
Rafin Rahman <ra-fin@live.com>
2021-02-04 11:18:39 +0000

    installed basic packages

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+// Test get request
+app.get('/',(req,res) => res.send('API Running'))
+// If there's no environment, it runs on port 5000
+const PORT = process.env.PORT || 5000
+// Callback once it connects to the PORT
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
