2a91c598755becd775230abc9fdf8db7678684f8
Rafin Rahman <ra-fin@live.com>
2021-02-04 14:51:28 +0000

    mongo db connected

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const connectDB = require('./config/db')
 const app = express()
+
+// COnnect database
+connectDB()
 // Test get request
-app.get('/',(req,res) => res.send('API Running'))
+app.get('/', (req, res) => res.send('API Running'))
 // If there's no environment, it runs on port 5000
 const PORT = process.env.PORT || 5000
 // Callback once it connects to the PORT
