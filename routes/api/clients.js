8eb7a46c2caf7ba4042a0dcb25c226663b614a03
Rafin Rahman <ra-fin@live.com>
2021-02-28 16:06:30 +0000

    Client POST request added

--- routes/api/clients.js
+++ routes/api/clients.js
@@ -1,9 +1,78 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
 
-// @route   GET api/clients
-// @desc    Clients route
+const Client = require('../../models/Client')
+const User = require('../../models/User')
+const University = require('../../models/University')
+const Course = require('../../models/Course')
+
+// @route   POST api/clients
+// @desc    ADD a new client from Marketing Officer
 // @access  Public
-router.get('/', (req, res) => res.send('Clients route'))
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('surname', 'Surname is required').not().isEmpty(),
+    check('email', 'Type a valid email').isEmail(),
+    check('telephone', 'Type a valid telephone number').not().isEmpty(),
+    check('qualification', 'Select a qualification').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+      name,
+      surname,
+      email,
+      telephone,
+      qualification,
+      university,
+      course,
+      comment,
+      submittedBy,
+      intake,
+    } = req.body
+
+    let marketingOfficer = await User.findOne({ email: submittedBy })
+    let universityModel = await University.findOne({ name: university })
+    let courseModel = await Course.findOne({ name: course })
+
+    try {
+      if (!marketingOfficer) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'This Marketing Officer does not exists' }] })
+      }
+      if (!universityModel) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'This University does not exists' }] })
+      }
+
+      let client = new Client({
+        name,
+        surname,
+        email,
+        telephone,
+        qualification,
+        university: universityModel.id,
+        course: courseModel.id,
+        comment,
+        intake,
+      })
+
+      await client.save()
+      return res.status(200).send({ msg: 'New client added' })
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server Error')
+    }
+  }
+)
 
 module.exports = router
